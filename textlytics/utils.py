fe06986ba72a3e4ffae4cc08aa5bfaf718a5eb89
Lukasz Augustyniak <lukasz.augustyniak@pwr.edu.pl>
2016-03-24 10:20:51 +0100

    added directory for w2v models

--- textlytics/utils.py
+++ textlytics/utils.py
@@ -6,7 +6,7 @@ File with all necessary utilities for Complex Networks project.
 
 __author__ = 'Łukasz Augustyniak'
 
-from os import path
+from os import path, makedirs
 
 # main paths
 MAIN_DIRECTORY = path.dirname(path.dirname(__file__))
@@ -20,6 +20,7 @@ CRAWLED_JSON_PATH = path.join(DATA_PATH, 'crawled_json')
 IMBD_PATH = path.join(DATA_PATH, 'imdb')
 SEMEVAL_PATH = path.join(DATA_PATH, 'semeval')
 LEXICONS_PATH = path.join(DATA_PATH, 'lexicons')
+W2V_MODELS_PATH = path.join(DATA_PATH, 'w2v_models')
 
 # paths to results
 # TODO /home?
@@ -48,4 +49,3 @@ def get_main_directory(*p):
     :return: path to main folder of project
     """
     return path.join(MAIN_DIRECTORY, *p)
-
