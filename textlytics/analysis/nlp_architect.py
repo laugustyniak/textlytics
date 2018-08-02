4bfea7dd429a8ae482dc1d1c2420d45ff3d8e986
Łukasz Augustyniak <lukasz.augustyniak@spoken.com>
2018-08-02 14:58:10 +0200

    get metrics from eval scores

---
+++ textlytics/analysis/nlp_architect.py
@@ -0,0 +1,32 @@
+import pickle
+from collections import namedtuple
+from pathlib import Path
+from typing import Callable, Dict
+
+
+Metrics = namedtuple('Metrics', ['precision', 'recall', 'f1'])
+
+
+def print_models_info(models_info_path: Path, model_info_params: Dict[str, Callable]):
+    for model_info_path in models_info_path.glob('*'):
+        with open(model_info_path.as_posix(), 'rb') as model_info_file:
+            model_info = pickle.load(model_info_file)
+            for info, fn in model_info_params.items():
+                print(info + ': ' + fn(model_info[info]))
+            print()
+
+
+def get_model_metrics(models_info_path: Path) -> Dict[str, Metrics]:
+    metrics = {}
+    for model_info_path in models_info_path.glob('*'):
+        with open(model_info_path.as_posix(), 'rb') as model_info_file:
+            model_info = pickle.load(model_info_file)
+            model_name = Path(model_info['test_file']).stem
+            evals = model_info['eval'][1]['aspect']
+            metrics[model_name] = Metrics(*evals)
+    return metrics
+
+
+if __name__ == '__main__':
+    metrics = get_model_metrics(Path('/home/lukasz/github/nlp/nlp-architect/examples/aspect_extraction/models-baseline'))
+    pass
