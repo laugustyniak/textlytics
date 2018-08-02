f8a6b53493edca4932cbb06ad863ec34ce58525c
Łukasz Augustyniak <lukasz.augustyniak@spoken.com>
2018-08-02 16:45:08 +0200

    exemplary model info params and their cleaning methods

--- textlytics/analysis/nlp_architect.py
+++ textlytics/analysis/nlp_architect.py
@@ -3,8 +3,14 @@ from collections import namedtuple
 from pathlib import Path
 from typing import Callable, Dict
 
-
 Metrics = namedtuple('Metrics', ['precision', 'recall', 'f1'])
+
+MODEL_INFO_PARAMS = {
+    'train_file': lambda f_name: Path(f_name).name,
+    'eval': lambda results: str(results[1]['aspect']),
+    'epoch': str,
+    'external_embedding_model': str
+}
 
 
 def print_models_info(models_info_path: Path, model_info_params: Dict[str, Callable]):
@@ -21,12 +27,13 @@ def get_model_metrics(models_info_path: Path) -> Dict[str, Metrics]:
     for model_info_path in models_info_path.glob('*'):
         with open(model_info_path.as_posix(), 'rb') as model_info_file:
             model_info = pickle.load(model_info_file)
-            model_name = Path(model_info['test_file']).stem
+            model_name = Path(model_info['test_file']).stem.replace('-test', '')
             evals = model_info['eval'][1]['aspect']
             metrics[model_name] = Metrics(*evals)
     return metrics
 
 
 if __name__ == '__main__':
-    metrics = get_model_metrics(Path('/home/lukasz/github/nlp/nlp-architect/examples/aspect_extraction/models-baseline'))
+    metrics = get_model_metrics(
+        Path('/home/lukasz/github/nlp/nlp-architect/examples/aspect_extraction/models-baseline'))
     pass
